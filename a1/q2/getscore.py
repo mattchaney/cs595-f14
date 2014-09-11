2f466596fc1fb1dee9e2653103d9421ebde85987
Matt Chaney <chaney.mathew@gmail.com>
2014-09-11 11:57:31 -0400

    adding additional error checking for getscore.py

--- a1/q2/getscore.py
+++ a1/q2/getscore.py
@@ -8,19 +8,32 @@ from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
 	if len(sys.argv) != 4:
-		print "Usage:\n\tpython getscore.py [school] [seconds] [uri]\n"
+		print "Usage:\n\tpython getscore.py [school] [period] [uri]\n"
 		sys.exit()
 	school = sys.argv[1]
-	period = float(sys.argv[2])
+	try:
+		period = float(sys.argv[2])
+	except ValueError:
+		print "[period] parameter must be valid floating point number"
+		sys.exit(1)
 	uri = sys.argv[3]
 	while True:
-		soup = BeautifulSoup(requests.get(uri).content)
+		try:
+			response = requests.get(uri)
+		except Exception as e:
+			print e
+			sys.exit(1)
+		soup = BeautifulSoup(response.content)
 		gametag = soup.find('em', text=re.compile('^'+school+'$'))
 		if not gametag:
 			print "Game not found"
 			sys.exit()
 		game = gametag.parent.parent.parent
-		teams = [game.find_all('span', {'class':'team'})[0].em.text, game.find_all('span', {'class':'team'})[1].em.text]
-		scores = [game.find('span', {'class':'away'}).text, game.find('span', {'class':'home'}).text]
+		try:
+			teams = [game.find_all('span', {'class':'team'})[0].em.text, game.find_all('span', {'class':'team'})[1].em.text]
+			scores = [game.find('span', {'class':'away'}).text, game.find('span', {'class':'home'}).text]
+		except:
+			print "Game not found"
+			sys.exit()
 		print teams[0] + ' ' + scores[0] + ', ' + teams[1] + ' ' + scores[1]
 		time.sleep(period)
