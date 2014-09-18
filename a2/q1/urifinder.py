7841bfecf26489b7a2e89e6aa1003418b2be427e
Matt Chaney <chaney.mathew@gmail.com>
2014-09-18 12:47:54 -0400

    finished a2/q1

--- a2/q1/urifinder.py
+++ a2/q1/urifinder.py
@@ -1,10 +1,7 @@
 # -*- encoding: utf-8 -*-
-from __future__ import unicode_literals
 import requests
 from requests_oauthlib import OAuth1
-from urlparse import parse_qs
 from urllib import quote
-from pprint import pprint
 
 REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token"
 AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token="
@@ -18,29 +15,61 @@ OAUTH_TOKEN_SECRET = "tGYCQa9LL2i6wmApJzbGzHdVIVA65xiwVffPmbqWJwZPs"
 
 SEARCH_URI = "https://api.twitter.com/1.1/search/tweets.json?q="
 
-SEARCH_ITEMS = map(quote, ['space x', 
-                           'elon musk', 
-                           'richard garriott', 
-                           'starcraft', 
-                           'ebola virus',
-                           'space balls',
-                           'space ghost',
-                           'rick and morty',
-                           'ren and stimpy',
-                           'android'])
-
-def search():
-    for item in SEARCH_ITEMS: 
-        result = requests.get(SEARCH_URI + item + '&filter%3Alinks', auth=oauth)
+SEARCH_ITEMS = map(quote, [ 'space x', 
+							'elon musk', 
+							'richard garriott', 
+							'starcraft 2', 
+							'ebola virus',
+							'world cup',
+							'singularity',
+							'rick and morty',
+							'iphone 6',
+							'android',
+							'robin williams',
+							'tony stewart',
+							'bitcoin',
+							'game of thrones',
+							'facebook',
+							'youtube',
+							'google',
+							'chris roberts',
+							'hyper light drifter',
+							'golang'])
 
 def get_oauth():
-    oauth = OAuth1(CONSUMER_KEY,
+	return OAuth1(CONSUMER_KEY,
                 client_secret=CONSUMER_SECRET,
                 resource_owner_key=OAUTH_TOKEN,
                 resource_owner_secret=OAUTH_TOKEN_SECRET)
-    return oauth
+
+def find_uris(uris):
+	for search_item in SEARCH_ITEMS:
+		result = requests.get(SEARCH_URI + search_item + '&filter%3Alinks&count=1000', auth=oauth)
+		for status in result.json()['statuses']:
+			for url in status['entities']['urls']:
+				if len(uris) == 1000:
+					return
+				if 'expanded_url' in url:
+					try:
+						result = requests.get(url['expanded_url'])
+						# only add expanded uris if they aren't in the list already
+						if result.status_code == 200 and result.url not in uris:
+							add_uri(uris, result.url)
+					except Exception as e:
+						print e
+						continue
+
+def add_uri(uris, uri):
+	uris.add(uri)
+	print 'added uri #%d: %s' % (len(uris), uri)
 
 if __name__ == "__main__":
-    oauth = get_oauth()
-    r = requests.get(url="https://api.twitter.com/1.1/statuses/mentions_timeline.json", auth=oauth)
-    print r.json()
+	oauth = get_oauth()
+	uris = set()
+	# read in previous set of uris
+	with open('output', 'r') as infile:
+		for line in infile.readlines():
+			add_uri(uris, line.strip())
+	find_uris(uris)
+	with open('output', 'w') as outfile:
+		outfile.writelines('%s\n' % uri for uri in uris)
