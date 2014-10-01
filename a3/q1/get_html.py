426dcec65c8128e3a60601aa78b595fe87942aec
Matt Chaney <chaney.mathew@gmail.com>
2014-10-01 14:07:14 -0400

    Adding html element removal as well

--- a3/q1/get_html.py
+++ a3/q1/get_html.py
@@ -3,6 +3,8 @@
 import requests
 import futures
 import md5
+from bs4 import BeautifulSoup
+import sys
 
 def convert(uri):
 	return md5.new(uri).hexdigest()
@@ -17,19 +19,24 @@ if __name__ == '__main__':
 		uris = [uri.rstrip('\n') for uri in infile]
 
 	with futures.ThreadPoolExecutor(max_workers=8) as executor:
-		with open('html/uri_map', 'w') as map_file:
-			uri_futures = [executor.submit(get_html, uri) for uri in uris]
-			for future in futures.as_completed(uri_futures):
+		uri_futures = [executor.submit(get_html, uri) for uri in uris]
+		for future in futures.as_completed(uri_futures):
+			try:
+				uri, status_code, content = future.result()
+			except Exception as exc:
+				print('{} generated an exception: {}'.format(uri, exc))
+				continue
+			if status_code == 200:
+				hashed_uri = convert(uri)
+				print('Writing {} as {}'.format(uri, hashed_uri))
 				try:
-					uri, status_code, content = future.result()
-				except Exception as exc:
-					print('{} generated an exception: {}'.format(uri, exc))
-					continue
-				if status_code == 200:
-					hashed_uri = convert(uri)
-					print('Writing {} as {}'.format(uri, hashed_uri))
-					map_file.write('{} {}\n'.format(uri, hashed_uri))
 					with open('html/raw/' + hashed_uri, 'w') as outfile:
 						outfile.write(content)
-				else:
-					print('Not writing {}, bad status code: {}'.format(uri, status_code))
+					with open('html/processed/' + hashed_uri, 'w') as outfile:
+						outfile.write(BeautifulSoup(content).get_text().encode('utf8'))
+				except Exception as e:
+					print '**** ERROR **** --- ' + uri
+					print e
+
+			else:
+				print('Not writing {}, bad status code: {}'.format(uri, status_code))
