fca482fc8cd475df1d07ef3f27a80253eb566968
Matt Chaney <chaney.mathew@gmail.com>
2014-11-04 18:12:44 -0500

    adding transition to separate groups

--- a7/q1/convert.py
+++ a7/q1/convert.py
@@ -1,62 +1,48 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 
 import pickle
-
-FILENAME = 'karate.pickle'
-
-data = pickle.loads(open(FILENAME).read())['karate']
-factions = data.vs['Faction']
+import json
 
 def build_matrix(raw, n):
+	"""Builds a matrix from a list of strings where each string in
+	is a space-separated row of the matrix"""
 	matrix = [[0 for y in range(n)] for x in range(n)]
 	for idx, line in enumerate(raw):
 		for idy, val in enumerate(line.split()):
 			matrix[idx][idy] = int(val)
 	return matrix
 
-def write_nodes(output, graph):
-	node = {}
-	for idx, club in enumerate(factions):
-		output.write("\t\t{ \"id\":%d, \"club\":%d }" % (idx, club))
-		if idx+1 != len(factions):
-			output.write(',')
-		output.write('\n')
+def build_nodes(clubs, names):
+	"""Builds a list of nodes represented as a python dict"""
+	return [{"id": i, "club": c, "name": names[i]} for i, c in enumerate(clubs)]
 
-
-def write_links(output, egraph, cgraph):
+def build_links(egraph, cgraph):
+	"""Builds a list of links represented as a python dict"""
+	links = []
 	for idx, line in enumerate(egraph):
 		for idy, val in enumerate(line):
 			if val == 1:
-				output.write("\t\t{ \"source\":%d, \"target\":%d, \"value\":%d }" % (idx, idy, cgraph[idx][idy]))
-				if idy+1 != len(line):
-					output.write(',')
-				output.write('\n')
+				links.append({"source": idx, "target": idy, "value": cgraph[idx][idy]})
+	return links
 
 if __name__ == '__main__':
+	filename = 'karate.pickle'
+	data = pickle.loads(open(filename).read())['karate']
+	clubs = data.vs['Faction']
+	names = data.vs['name']
+
+	# Read lines from input data file
 	lines = [line.strip() for line in open('karate.txt').readlines()]
 
-	# get length of line
-	n = int(lines[1].split()[0].replace('N=',''))
+	# parse size of nxn matrix
+	n = int(lines[1].split()[0].replace('N=', ''))
 
-	ename = lines[4]
+	# build matrices using input data
 	egraph = build_matrix(lines[7:41], n)
-
-	cname = lines[5]
 	cgraph = build_matrix(lines[41:], n)
 
+	m = {}
+	m['nodes'] = build_nodes(clubs, names)
+	m['links'] = build_links(egraph, cgraph)
 	with open('out.json', 'w') as output:
-		output.write('{\n')
-		output.write('\t"graph": [\n\t\t["name", "Zachary\'s Karate Club"]\n\t],\n')
-		
-		output.write('\t"nodes": [\n')
-		# write nodes
-		write_nodes(output, egraph)
-		output.write('\t],\n')
-		output.write('\t"links": [\n')
-
-		# write links
-		write_links(output, egraph, cgraph)
-		output.write('\t],\n')
-
-		output.write("\t\"multigraph\": false\n")
-		output.write('}\n')
+		output.write(json.dumps(m, indent=1, separators=(',', ': ')))
