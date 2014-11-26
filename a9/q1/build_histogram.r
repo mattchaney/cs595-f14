077eaae4651a3df29d51adf269890b6a23c7fa5e
Matt Chaney <chaney.mathew@gmail.com>
2014-11-26 15:57:41 -0500

    adding incomplete report

--- a9/q1/build_histogram.r
+++ a9/q1/build_histogram.r
@@ -3,5 +3,5 @@
 data <- read.table("pagecounts", sep="\t", header=TRUE, comment.char="")
 counts <- table(data$pages)
 pdf("hist.pdf")
-barplot(counts, ylab="Sites", xlab="Page Count", main="Page Count per Site")
+barplot(counts, ylab="Number of Blogs", xlab="Page Count", main="Page Count per Blog")
 dev.off()
